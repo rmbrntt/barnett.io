4f7ddab5ca8c8b1aac8204149c4f49111f219c65
Ryan <ryan@barnett.io>
2017-03-29 21:02:14 -0400

    added django article model, serializer and setup endpoint for react to fetch articles

--- api/blog/models.py
+++ api/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    slug = models.SlugField(unique=True, editable=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+

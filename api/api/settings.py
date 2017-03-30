af7b97320f8f49468c9d4a0ae6a2439aebd21246
Ryan <ryan@barnett.io>
2017-03-29 21:44:52 -0400

    added django CORS headers and made successful API call from React, need to parse the JSON into props

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'blog'
+    'blog',
+    'corsheaders',
 ]
 
 
@@ -50,7 +51,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+    '127.0.0.1:8000',
+    '127.0.0.1:3000',
+    'localhost:3000',
+    'barnett.io',
+)
 
 ROOT_URLCONF = 'api.urls'
 
@@ -69,6 +80,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'api.wsgi.application'
 

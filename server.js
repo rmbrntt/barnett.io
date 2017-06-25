66320f82acecef13d19ce296232e4e8c6730748b
Ryan Barnett <rmbrntt@gmail.com>
2017-06-25 00:49:37 -0400

    reconfigure node

--- server.js
+++ server.js
@@ -1,69 +1,27 @@
-import express from 'express';
-import morgan from 'morgan';
+// import express from 'express';
+// import morgan from 'morgan';
+//
+//
+// const app = express();
+//
+// app.set('port', (process.env.API_PORT || 3001));
+//
+// if (process.env.NODE_ENV !== 'TEST') {
+//   app.use(morgan('combined'));
+// }
+//
+// app.use('/', express.static(path.join(__dirname, '/client/build')));
+//
 
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.set('port', (process.env.API_PORT || 3001));
+app.set('port', (8080));
 
-if (process.env.NODE_ENV !== 'TEST') {
-  app.use(morgan('combined'));
-}
-
-// A fake API token we validate against
-export const API_TOKEN = 'D6W69PRgCoDKgHZGJmRUNA';
-
-const extractToken = (req) => (
-  req.query.token
-);
-
-const authenticatedRoute = ((req, res, next) => {
-  const token = extractToken(req);
-
-  if (token) {
-    if (token === API_TOKEN) {
-      return next();
-    } else {
-      return res.status(403).json({
-        success: false,
-        error: 'Invalid token provided',
-      });
-    }
-  } else {
-    return res.status(403).json({
-      success: false,
-      error: 'No token provided. Supply token as query param `token`',
-    });
-  }
+app.use('/', express.static(path.join(__dirname, '/build')));
+app.listen(app.get('port'), () => {
+  console.log(`Server running at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
-
-app.get('/api/check_token', (req, res) => {
-  const token = extractToken(req);
-
-  if (token) {
-    if (token === API_TOKEN) {
-      return res.json({ valid: true });
-    } else {
-      return res.json({ valid: false });
-    }
-  } else {
-    return res.status(400).json({
-      valid: false,
-      error: 'No token found in `Authorization` header',
-    });
-  }
-});
-
-// Make things more noticeable in the UI by introducing a fake delay
-// to logins
-const FAKE_DELAY = 500; // ms
-app.post('/api/login', (req, res) => {
-  setTimeout(() => (
-    res.json({
-      success: true,
-      token: API_TOKEN,
-    })
-  ), FAKE_DELAY);
-});
-
-export default app;
